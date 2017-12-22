811eb100d373c53b85748de02e5a3822c193c257
Bernd Doser <bernd.doser@braintwister.eu>
2017-12-22 10:06:15 +0100

    correct cdt/docker plugin

--- ubuntu-16.04-cmake-3.9-gcc-7-conan-0.29.1-eclipse-4.7.2/install_plugins.sh
+++ ubuntu-16.04-cmake-3.9-gcc-7-conan-0.29.1-eclipse-4.7.2/install_plugins.sh
@@ -5,8 +5,9 @@ $INSTALLATION_DIR/eclipse/eclipse \
 -application org.eclipse.equinox.p2.director \
 -destination $INSTALLATION_DIR/eclipse \
 -repository \
-https://raw.githubusercontent.com/15knots/cmakeed/master/cmakeed-update/ \
+https://raw.githubusercontent.com/15knots/cmakeed/master/cmakeed-update/,\
+http://download.eclipse.org/tools/cdt/releases/9.4/ \
 -installIU \
-com.cthing.cmakeed.feature.feature.group \
+com.cthing.cmakeed.feature.feature.group,\
 org.eclipse.cdt.docker.launcher.source.feature.group \
 -tag docker_initial
