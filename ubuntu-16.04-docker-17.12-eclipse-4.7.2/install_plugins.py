5d3afa597fc054c2394669dd2e2f9494a8206a7c
Bernd Doser <bernd.doser@braintwister.eu>
2018-01-10 14:16:18 +0100

    improve plugin installation eclipse and add arduino

--- ubuntu-16.04-docker-17.12-eclipse-4.7.2/install_plugins.py
+++ ubuntu-16.04-docker-17.12-eclipse-4.7.2/install_plugins.py
@@ -6,14 +6,14 @@ import yaml
 stream = open('plugins.yml', 'r')
 plugins = yaml.load(stream)
 
-for plugin in plugins:
-  cmd = "$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse -repository " \
-      + plugin['repo'] \
-      + " -installIU"
-
-  for unit in plugin['units']:
-    cmd += " " + unit
-
-  cmd += " -tag docker_initial"
+for plugin_name, feature_group in plugins.items():
+  print(plugin_name)
+  for features in feature_group:
+    cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse -repository ' \
+        + features['repo'] \
+        + ' -installIU' \
+        + ' ' + ','.join(features['units']) \
+        + ' -tag docker_initial'
   
-  subprocess.run(cmd, shell=True, check=True)
+    print(cmd)
+    #subprocess.run(cmd, shell=True, check=True)
