6c0f97e0bc200fc79c9eef6df8abfb7098191080
Bernd Doser <bernd.doser@braintwister.eu>
2018-01-16 15:28:01 +0100

    change eclipse user to root

--- ubuntu-16.04-cmake-3.10-gcc-7-conan-1.0.1-eclipse-4.7.2/install_plugins.py
+++ ubuntu-16.04-cmake-3.10-gcc-7-conan-1.0.1-eclipse-4.7.2/install_plugins.py
@@ -10,7 +10,7 @@ for plugin_name, feature_group in plugins.items():
   print(plugin_name)
   for features in feature_group:
     cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
-        + ' -application org.eclipse.equinox.p2.director -destination /home/eclipse/.eclipse' \
+        + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
         + ' -repository ' + features['repo'] \
         + ' -installIU '  + ','.join(features['units']) \
         + ' -tag docker_initial'
