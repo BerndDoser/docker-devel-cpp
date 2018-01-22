c032a899270cc7272d0d501d0a9c5f2b36bd0461
Bernd Doser <bernd.doser@braintwister.eu>
2018-01-22 14:04:52 +0100

    Fix #2: Eclipse plugins can not be updated
    Updates will be installed during container build. Since the p2 director has no
    update function, the plugins of the eclipse package must be uninstalled before
    reinstallation. Therefore, a new section 'uninstall' was added to
    'plugins.yml'.
    
    Manual update procedures using the "Check for Updates' button is not
    recommended, because the changes are not persistent.

--- ubuntu-16.04-cmake-3.10-gcc-7-conan-1.0-docker-17.12-eclipse-4.7.2/install_plugins.py
+++ ubuntu-16.04-cmake-3.10-gcc-7-conan-1.0-docker-17.12-eclipse-4.7.2/install_plugins.py
@@ -6,13 +6,20 @@ import yaml
 stream = open('plugins.yml', 'r')
 plugins = yaml.load(stream)
 
-for plugin_name, feature_group in plugins.items():
-  print(plugin_name)
+# Uninstall plugins
+cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
+    + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
+    + ' -uninstallIU '  + ','.join(plugins['uninstall']) \
+
+subprocess.run(cmd, shell=True, check=True)
+
+# Install plugins
+for plugin_name, feature_group in plugins['install'].items():
+  print('Install ', plugin_name)
   for features in feature_group:
     cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
         + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
         + ' -repository ' + features['repo'] \
-        + ' -installIU '  + ','.join(features['units']) \
-        + ' -tag docker_initial'
+        + ' -installIU '  + ','.join(features['units'])
   
     subprocess.run(cmd, shell=True, check=True)
